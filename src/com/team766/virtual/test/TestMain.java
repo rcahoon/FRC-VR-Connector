a7068064c36b0b91805a15a1a765b17117e4d208
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2015-11-01 02:45:37 -0800

    Remove RESET_SIM hack

--- src/com/team766/virtual/test/TestMain.java
+++ src/com/team766/virtual/test/TestMain.java
@@ -8,21 +8,13 @@ public class TestMain {
 	public static void main(String[] args) {
 		try {
 			VRConnector vrc = new VRConnector();
-			for (int i = 0; i < 10; ++i) {
-				vrc.putCommandBool(VRConnector.RESET_SIM, true);
-				vrc.process();
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
+			vrc.putCommandBool(VRConnector.RESET_SIM, true);
 			while (true) {
 				vrc.putCommand(VRConnector.LEFT_MOTOR, 256);
 				vrc.putCommand(VRConnector.RIGHT_MOTOR, 512);
 				vrc.putCommand(VRConnector.INTAKE, -1);
 				if (vrc.process()) {
-	                System.out.println("Left enc: " + vrc.getFeedback(VRConnector.LEFT_ENCODER));
+					System.out.println("Left enc: " + vrc.getFeedback(VRConnector.LEFT_ENCODER));
 					System.out.println("Right enc: " + vrc.getFeedback(VRConnector.RIGHT_ENCODER));
 					System.out.println("Heading: " + vrc.getFeedback(VRConnector.HEADING));
 					System.out.println("Intake State: " + vrc.getFeedback(VRConnector.INTAKE_STATE));
